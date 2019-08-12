819e812fa47b42ff246c7cc92e5f301761fc8cce
w.benarto <w.benarto@gmail.com>
2019-08-11 18:41:32 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Navbar />
+    )
+  };
+}
+
+export default App;
