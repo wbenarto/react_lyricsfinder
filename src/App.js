1401fcc0245a62aa3635917b077d954231ddda26
w.benarto <w.benarto@gmail.com>
2019-08-12 20:49:38 -0700

    Lyrics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import Navbar from "./components/layout/Navbar";
 import Index from './components/layout/Index';
+import Lyrics from './components/tracks/Lyrics';
+
 import { Provider } from './context'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 class App extends Component {
@@ -15,6 +18,7 @@ class App extends Component {
             <div className='container'>
               <Switch>
                 <Route exact path='/' component={Index} />
+                <Route exact path='/lyrics/tracks/:id' component={Lyrics} />
               </Switch>
             </div>
         </React.Fragment>
